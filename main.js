c11b4321b970e4f972779b54b6187a4b2f8950aa
NNECHI ANGELA UCHECHI <angelannechi@gmail.com>
2025-01-11 23:57:29 +0100

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 "use script";
+document.querySelector(".menu-icon").addEventListener("click", function () {
+  const menuLinks = document.querySelector(".nav-bar");
+  menuLinks.classList.toggle("show");
+  const icon = document.querySelector("#barz");
+});
