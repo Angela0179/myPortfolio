f0ff25122ce510b718dd04280468584c99cd4b8d
NNECHI ANGELA UCHECHI <angelannechi@gmail.com>
2025-01-19 17:25:51 +0100

    last commit

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ document.querySelector(".menu-icon").addEventListener("click", function () {
   menuLinks.classList.toggle("show");
   const icon = document.querySelector("#barz");
 });
+
+let header = document.getElementById("navigation");
+let btns = header.getElementsByClassName("btn");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
