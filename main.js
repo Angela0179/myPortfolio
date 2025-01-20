71abd2ea66de8365a17392243154030e543094e3
NNECHI ANGELA UCHECHI <angelannechi@gmail.com>
2025-01-20 07:34:58 +0100

    final commit

--- main.js
+++ main.js
@@ -5,12 +5,38 @@ document.querySelector(".menu-icon").addEventListener("click", function () {
   const icon = document.querySelector("#barz");
 });
 
-let header = document.getElementById("navigation");
-let btns = header.getElementsByClassName("btn");
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    let current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+// // Get the container element
+// var myContainer = document.getElementById("navigation");
+// let sections = document.
+
+// // Get all buttons with class="btn" inside the container
+// var tags = myContainer.getElementsByClassName("tag");
+
+// // Loop through the buttons and add the active class to the current/clicked button
+// for (var i = 0; i < btns.length; i++) {
+//   tags[i].addEventListener("click", function () {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
+let sections = document.querySelector("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classlist.add("active");
+      });
+    }
   });
-}
+};
